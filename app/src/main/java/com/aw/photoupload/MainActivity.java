68eabf43cce277b1006929640acce282b1514f4e
Alex Kim <akim316@gatech.edu>
2015-02-28 12:21:32 -0500

    Second commit

--- app/src/main/java/com/aw/photoupload/MainActivity.java
+++ app/src/main/java/com/aw/photoupload/MainActivity.java
@@ -1,17 +1,53 @@
 package com.aw.photoupload;
 
+import android.app.ActionBar;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableRow;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+
+import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 
 public class MainActivity extends ActionBarActivity {
+    private Button button;
+    private TimePicker timePickerFrom;
+    private TimePicker timePickerTo;
+    private TableRow pickerRow;
+    private TableRow timeTextRow;
+    private EditText nameField;
+    private SimpleDateFormat sdf;
+    private GregorianCalendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(cameraButtonOnClickListener);
+        nameField = (EditText) findViewById(R.id.nameField);
+        timePickerFrom = (TimePicker) findViewById(R.id.timePickerFrom);
+        timePickerTo = (TimePicker) findViewById(R.id.timePickerTo);
+        pickerRow = (TableRow) findViewById(R.id.timePickerRow);
+        timeTextRow = (TableRow) findViewById(R.id.timeText);
+        timeTextRow.setVisibility(View.GONE);
+        calendar = new GregorianCalendar();
+
     }
 
 
@@ -36,4 +72,25 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public View.OnClickListener cameraButtonOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            pickerRow.setVisibility(View.GONE);
+            timeTextRow.setVisibility(View.VISIBLE);
+            Intent imageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            File folder = new File("/sdcard/pics/");
+            folder.mkdirs();
+            if (timePickerFrom.getCurrentHour() <= calendar.get(Calendar.HOUR_OF_DAY)
+                    && timePickerFrom.getCurrentMinute() <= calendar.get(Calendar.MINUTE)
+                    && timePickerTo.getCurrentHour() >= calendar.get(Calendar.HOUR_OF_DAY)
+                    && timePickerTo.getCurrentMinute() >= calendar.get(Calendar.MINUTE)) {
+                Uri uriSavedImage = Uri.fromFile(new File("/sdcard/pics/" + nameField.getText() + "_"
+                        + sdf.format(new Date()) + ".png"));
+                imageIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+            }
+            startActivityForResult(imageIntent, 0);
+        }
+    };
+
 }
